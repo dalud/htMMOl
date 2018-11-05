d96177afb76a6ea47d754e8e14b03da53be8c926
dalud <jaakko.niska@gmail.com>
2018-11-05 16:00:10 +0200

    working on mapReader.php TODO: conduct more AI pathFinding tests

--- mapReader.php
+++ mapReader.php
@@ -4,4 +4,9 @@
  * User: 100029006
  * Date: 2.11.2018
  * Time: 12.43
+ *
+ * int imagecolorat ( resource $image , int $x , int $y )
  */
+
+$imageSize = getimagesize('map.png');
+echo $imageSize[0];
