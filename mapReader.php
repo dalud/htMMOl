1b6745c7ff58a2b1a0b68d53c1245d0fd7b856c1
dalud <jaakko.niska@gmail.com>
2018-11-06 15:46:39 +0200

    working on mapReader.php TODO: conduct more AI pathFinding tests

--- mapReader.php
+++ mapReader.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: 100029006
- * Date: 2.11.2018
- * Time: 12.43
- *
  * int imagecolorat ( resource $image , int $x , int $y )
  */
 
+$img = imagecreatefrompng('map.png');
+
 $imageSize = getimagesize('map.png');
-echo $imageSize[0];
+$width = $imageSize[0];
+$height = $imageSize[1];
+
+for($y = 0; $y < $height; $y++){
+    for($x = 0; $x < $width; $x++){
+        echo imagecolorat($img, $x, $y);
+    }
+}
